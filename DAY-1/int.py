5bbcfe79a4af057345aebb33bf8d78f550ddac73
Sakshyam0426 <sakshyam.paudel5@gmail.com>
2026-05-21 08:38:19 +0545

    Adding files

---
+++ DAY-1/int.py
@@ -0,0 +1,17 @@
+# int data Type 
+
+
+
+# Declaring int variable
+
+x = 10
+y = 5
+
+# Performing arithmetic operatinos on int data
+
+print(f"\n Arithmetic Operations")
+print(f"\n Sum: {x + y}")
+print(f"\n Difference: {x - y}")
+print(f"\n Product: {x * y} ")
+print(f"\n Quotient: {x / y}")
+print(f"\n Exponent: {x ** y}")
